6108e30869700854c522382d65b5e61e1cb42af8
Sérgio Vieira <sergiosvieira@gmail.com>
2013-06-19 09:14:03 -0300

    Initial Commit

---
+++ WorkSchedule/BRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BRAppDelegate.h
+//  WorkSchedule
+//
+//  Created by sergio vieira on 19/06/13.
+//  Copyright (c) 2013 sergio vieira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WorkSchedule/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WorkSchedule
+//
+//  Created by sergio vieira on 19/06/13.
+//  Copyright (c) 2013 sergio vieira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BRAppDelegate class]));
+    }
+}
